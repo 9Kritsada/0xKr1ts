a83f59a1d43785a9817c56e1a14da96e86bf9cf4
9Kritsada <kritsada.wiriya@gmail.com>
2024-09-24 08:44:22 +0700

    First Update and Test Template

--- src/config.ts
+++ src/config.ts
@@ -7,8 +7,8 @@ import type {
 import { LinkPreset } from './types/config'
 
 export const siteConfig: SiteConfig = {
-  title: 'Fuwari',
-  subtitle: 'Demo Site',
+  title: '0xKr1ts',
+  subtitle: 'Profile',
   lang: 'en',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko'
   themeColor: {
     hue: 250,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
@@ -47,13 +47,13 @@ export const navBarConfig: NavBarConfig = {
 }
 
 export const profileConfig: ProfileConfig = {
-  avatar: 'assets/images/demo-avatar.png',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-  name: 'Lorem Ipsum',
-  bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  avatar: 'assets/images/0xKrits.png',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+  name: '0xKr1ts',
+  bio: '0x16 0x14',
   links: [
     {
       name: 'Twitter',
-      icon: 'fa6-brands:twitter',       // Visit https://icones.js.org/ for icon codes
+      icon: 'fa6-brands:twitter',       // Visit https://icones.js.org/ for icon codes https://icones.js.org/collection/fa6-brands
                                         // You will need to install the corresponding icon set if it's not already included
                                         // `pnpm add @iconify-json/<icon-set-name>`
       url: 'https://twitter.com',
@@ -66,6 +66,21 @@ export const profileConfig: ProfileConfig = {
     {
       name: 'GitHub',
       icon: 'fa6-brands:github',
+      url: 'https://github.com/saicaca/fuwari',
+    },
+    {
+      name: 'GitHub',
+      icon: 'fa6-brands:instagram',
+      url: 'https://github.com/saicaca/fuwari',
+    },
+    {
+      name: 'GitHub',
+      icon: 'fa6-brands:facebook',
+      url: 'https://github.com/saicaca/fuwari',
+    },
+    {
+      name: 'GitHub',
+      icon: 'fa6-brands:facebook',
       url: 'https://github.com/saicaca/fuwari',
     },
   ],

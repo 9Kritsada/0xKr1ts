5fdd8a2bdf4c74ea34d452ef21e36a17fa0dc6ae
9Kritsada <kritsada.wiriya@gmail.com>
2024-10-22 17:12:38 +0700

    Update Theme and Profile

--- src/config.ts
+++ src/config.ts
@@ -11,7 +11,7 @@ export const siteConfig: SiteConfig = {
   subtitle: 'Profile',
   lang: 'en',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko'
   themeColor: {
-    hue: 250,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
+    hue: 0,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
     fixed: true,     // Hide the theme color picker for visitors
   },
   banner: {
@@ -26,7 +26,7 @@ export const siteConfig: SiteConfig = {
   },
   favicon: [    // Leave this array empty to use the default favicon
     {
-      src: '/favicon/GOAT2.png',    // Path of the favicon, relative to the /public directory
+      src: '/favicon/Redd.png',    // Path of the favicon, relative to the /public directory
       sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
     }
   ]
@@ -46,7 +46,7 @@ export const navBarConfig: NavBarConfig = {
 }
 
 export const profileConfig: ProfileConfig = {
-  avatar: 'assets/images/GOAT2.webp',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+  avatar: 'assets/images/Redd.png',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
   // avatar: 'assets/images/0xkr1ts.png',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
   name: '0xKr1ts',
   bio: 'whoami',
--- src/utils/setting-utils.ts
+++ src/utils/setting-utils.ts
@@ -12,19 +12,19 @@ export function getDefaultHue(): number {
   return Number.parseInt(configCarrier?.dataset.hue || fallback)
 }
 
-export function getHue(): number {
-  const stored = localStorage.getItem('hue')
-  return stored ? Number.parseInt(stored) : getDefaultHue()
-}
+// export function getHue(): number {
+//   const stored = localStorage.getItem('hue')
+//   return stored ? Number.parseInt(stored) : getDefaultHue()
+// }
 
-export function setHue(hue: number): void {
-  localStorage.setItem('hue', String(hue))
-  const r = document.querySelector(':root') as HTMLElement
-  if (!r) {
-    return
-  }
-  r.style.setProperty('--hue', String(hue))
-}
+// export function setHue(hue: number): void {
+//   localStorage.setItem('hue', String(hue))
+//   const r = document.querySelector(':root') as HTMLElement
+//   if (!r) {
+//     return
+//   }
+//   r.style.setProperty('--hue', String(hue))
+// }
 
 export function applyThemeToDocument(theme: LIGHT_DARK_MODE) {
   switch (theme) {

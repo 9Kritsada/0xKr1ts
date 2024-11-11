6d90b54ab03a6e93fde949570922aae8221e3b33
9Kritsada <kritsada.wiriya@gmail.com>
2024-11-12 00:01:32 +0700

    Set time reading

--- src/plugins/remark-reading-time.mjs
+++ src/plugins/remark-reading-time.mjs
@@ -5,7 +5,9 @@ import getReadingTime from 'reading-time'
 export function remarkReadingTime() {
   return (tree, { data }) => {
     const textOnPage = toString(tree)
-    const readingTime = getReadingTime(textOnPage)
+    const readingTime = getReadingTime(textOnPage, {
+      wordsPerMinute: 75
+    })
     data.astro.frontmatter.minutes = Math.max(
       1,
       Math.round(readingTime.minutes),

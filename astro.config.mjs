f60458434a2cf47b9fc47ef2ea6af9e340290bcd
9Kritsada <kritsada.wiriya@gmail.com>
2024-12-05 01:48:28 +0700

    Change defineConfig site

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,7 +31,7 @@ const oklchToHex = (str) => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://krits.xyz/",
+  site: "https://krits.top/",
   base: "/",
   trailingSlash: "always",
   integrations: [
